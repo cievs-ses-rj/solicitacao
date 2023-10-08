62fefe70d8a893a3f9d7f6f34d4120c218cc4268
eduardompeixoto <46012106+eduardompeixoto@users.noreply.github.com>
2023-10-08 20:33:06 -0300

    Update esus.R

--- R/esus.R
+++ R/esus.R
@@ -7,7 +7,7 @@ require(readr)
 require(googlesheets4)
 googlesheets4::gs4_deauth()
 
-read_sheet("https://docs.google.com/spreadsheets/d/1AzDiYqHrYn3IHRTwHsCYi6MZRLHPFIZJeu2dCxzAI5M/edit#gid=0")
+read_sheet("https://docs.google.com/spreadsheets/d/1IV6o4wJ7BZxCIzjuSGMqEji6VemTd8u5c0IHJFdWi4A/edit?usp=sharing")
 
   
 
